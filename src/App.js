bade0c6d38ab5ec49969e39aeb75dd0595cd5488
kongchip <myunghwan0321@gmail.com>
2023-05-09 00:24:55 +0900

    메인 베너 및 상품 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
+import data from './data.js';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  let [shoes] = useState(data);
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Card shoes={shoes[i]} i={i} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
 }
 
 export default App;
