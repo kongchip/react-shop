5a24606d1a19de0093545314453a5ce9ad8eb164
kongchip <myunghwan0321@gmail.com>
2023-05-20 13:49:30 +0900

    detail페이지 생성및 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -2,32 +2,57 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
 import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/detail');
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-      <div className="App">
-        <div className="container">
-          <div className="row">
-            {shoes.map((a, i) => {
-              return <Card shoes={shoes[i]} i={i} />;
-            })}
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="main-bg"></div>
+              <div className="App">
+                <div className="container">
+                  <div className="row">
+                    {shoes.map((a, i) => {
+                      return <Card shoes={shoes[i]} i={i} />;
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
