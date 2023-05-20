75696d134eef458577874839f71cca577d31a928
kongchip <myunghwan0321@gmail.com>
2023-05-20 15:36:22 +0900

    Main페이지 에서 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,24 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Detail(props) {
   let { id } = useParams();
+  let [alert, setAlert] = useState(true);
   let productId = props.shoes.find((x) => x.id == id);
 
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
   return (
     <div>
       <div className="container">
+        {alert == true ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null}
         <div className="row">
           <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
