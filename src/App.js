72343e1dc79314229e191b7766038780c5e79d48
kongchip <myunghwan0321@gmail.com>
2023-05-30 16:32:23 +0900

    메인페이지 성능개선 (lazy)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
-import { useEffect, useState, lazy } from 'react';
+import { useEffect, useState, lazy, Suspense } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import ErrorPage from './routes/ErrorPage';
 import AboutPage from './routes/aboutPage';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
-import Cart from './routes/Cart';
-import Detail from './routes/Detail';
+const Detail = lazy(() => import('./routes/Detail'));
+const Cart = lazy(() => import('./routes/Cart'));
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -82,57 +82,60 @@ function App() {
           {result.data && result.data.name + '님'}
         </Nav>
       </Navbar>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <div className="main-container">
-                <div className="main-bg" />
-                <div className="lookItem">
-                  <div className="look-item-h1">최근 본 상품</div>
-                  {lookItem.map((a, i) => {
-                    return <Item lookItem={lookItem[i]} key={i} shoes={shoes[lookItem[i]]} />;
-                  })}
-                </div>
-              </div>
-              <div className="App">
-                <div className="container">
-                  <div className="row">
-                    {shoes.map((a, i) => {
-                      return <Card shoes={shoes[i]} i={i} key={i} />;
+
+      <Suspense fallback={<div>로딩중입니다</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <div className="main-container">
+                  <div className="main-bg" />
+                  <div className="lookItem">
+                    <div className="look-item-h1">최근 본 상품</div>
+                    {lookItem.map((a, i) => {
+                      return <Item lookItem={lookItem[i]} key={i} shoes={shoes[lookItem[i]]} />;
                     })}
                   </div>
-                  <button
-                    disabled={isDisabled}
-                    onClick={() => {
-                      axios
-                        .get(baseUrl)
-                        .then((res) => {
-                          let copy = [...shoes, ...res.data];
-                          setShoes(copy);
-                          countBtn();
-                        })
-                        .catch(() => {
-                          console.log('서버 연결 실패');
-                        });
-                    }}
-                  >
-                    더 보기
-                  </button>
+                </div>
+                <div className="App">
+                  <div className="container">
+                    <div className="row">
+                      {shoes.map((a, i) => {
+                        return <Card shoes={shoes[i]} i={i} key={i} />;
+                      })}
+                    </div>
+                    <button
+                      disabled={isDisabled}
+                      onClick={() => {
+                        axios
+                          .get(baseUrl)
+                          .then((res) => {
+                            let copy = [...shoes, ...res.data];
+                            setShoes(copy);
+                            countBtn();
+                          })
+                          .catch(() => {
+                            console.log('서버 연결 실패');
+                          });
+                      }}
+                    >
+                      더 보기
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          }
-        />
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/about" element={<AboutPage />}>
-          <Route path="member" element={<div>직원 정보</div>} />
-          <Route path="location" element={<div>위치 정보</div>} />
-        </Route>
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+            }
+          />
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/about" element={<AboutPage />}>
+            <Route path="member" element={<div>직원 정보</div>} />
+            <Route path="location" element={<div>위치 정보</div>} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
